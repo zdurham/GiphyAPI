bd62d79b41b34f46fb2e907b78af81b3ff9f40db
Zach Durham <ZachDurham@Zachs-MacBook-Pro.local>
2017-06-22 22:29:44 -0400

    initial files, some styling in html

---
+++ assets/javascript/app.js
@@ -0,0 +1,2 @@
+var movies = ["There Will Be Blood", "American Psycho", "Star Wars", "Indiana Jones", "The Grand Budapest Hotel", "Mad Max", "Drive", "Hot Fuzz", "Shaun of the Dead", "Lone Wolf and Cub", "Resevoir Dogs", "American Beauty"]
+
